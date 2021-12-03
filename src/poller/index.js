e8fd62feb6e74d78c6a4008383e0618ca767cd37
mprey <masonprey7@gmail.com>
2021-12-02 22:55:00 -0500

    Change to cron and fix endpoint response

--- src/poller/index.js
+++ src/poller/index.js
@@ -2,14 +2,16 @@ const axios = require('axios');
 const Account = require('../mongo/schemas/Account');
 const Networth = require('../mongo/schemas/Networth');
 const notifier = require('../notifier');
+const cron = require('node-cron');
 const { POLL_TIME } = require('dotenv').config().parsed
 
 const API_KEY = '96e0cc51-a62e-42ca-acee-910ea7d2a241';
 const ENDPOINT = `https://api.zapper.fi/v1/balances?api_key=${API_KEY}`
 
 const init = () => {
-    console.log("Beginning polling time for seconds:", POLL_TIME)
-    setInterval(poll, POLL_TIME * 1000);
+    console.log("Beginning poller for 8am and 8pm CST");
+    // poll();
+    cron.schedule("00 8,18 * * *", poll, { timezone: "America/Chicago" });
 }
 
 const poll = async () => {
@@ -20,8 +22,16 @@ const poll = async () => {
     const url = ENDPOINT + addresses.map(address => `&addresses[]=${address}`).join("");
 
     const { data } = await axios.get(url);
-   
-    const rawNetworks = data.split("\n").filter(item => item.length);
+
+    // Ugly code to account for non-json endpoint response...
+    const rawNetworks = data.split("\n").filter(item => {
+        return item.startsWith("data: {") || item.startsWith("{");
+    }).map(item => {
+       if (item.startsWith("data: {")) {
+           return item.split("ata: ")[1];
+       }
+       return item;
+    });
 
     const networks = rawNetworks.map(raw => JSON.parse(raw));
     const addressBalances = addresses.reduce((accum, next) => {
